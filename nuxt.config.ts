34a95667b067dac29e4609b08c6a289064568010
EnjoyKojima <majiko2627kojima@gmail.com>
2025-02-17 23:21:02 +0900

    compatibilityVersion 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
 
   modules: ['nitro-cloudflare-dev', '@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
   devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2024-11-01',
 
   nitro: {
