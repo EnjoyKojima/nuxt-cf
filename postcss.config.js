53064740714442931cb8cb62ac2598482f1cd607
EnjoyKojima <majiko2627kojima@gmail.com>
2025-05-25 20:45:00 +0900

    delete postcss

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'postcss-load-config'
-import postcssImport from 'postcss-import'
-
-export default defineConfig({
-  plugins: {
-    'postcss-import': {},
-    '@tailwindcss/postcss': {},
-  },
-})
